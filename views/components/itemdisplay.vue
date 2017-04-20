88da316c384c2de8ec466370d7f8114c0cdc9e1c
lephuongtu271094 <lephuongtu271094@gmail.com>
2017-04-20 17:34:27 +0700

    aa
    aa

---
+++ views/components/itemdisplay.vue
@@ -0,0 +1,21 @@
+<!-- TODO: Hãy viết thành một component hoàn chỉnh -->
+<template lang="html">
+   <div id="img">
+
+   <span v-for="item in items">
+   <img :src="'../public/image/' + item.photo" :alt="item.name" width="128" height="128">
+       <p>{{item.name}}</p>
+       <p>{{item.type}}</p>
+    </span>
+
+   </div>
+</template>
+
+<script>
+export default {
+   props: ['items']
+}
+</script>
+
+<style lang="css">
+</style>
